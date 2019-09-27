d2ce2573d1d75b55de084d165caf09347329bf70
Uyen Phuong <uyenphuong1898@gmai.com>
2019-09-28 03:45:07 +0700

    Application created.

---
+++ MoneyLover/MoneyLover.UI/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MoneyLover.UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        private void BtnSignIn_Click(object sender, RoutedEventArgs e)
+        {
+            SignIn signinWindow = new SignIn();
+            signinWindow.ShowDialog();
+            this.Hide();
+
+        }
+
+        private void BtnRegister_Click(object sender, RoutedEventArgs e)
+        {
+            Register registerWindow = new Register();
+            registerWindow.ShowDialog();
+            this.Hide();
+        }
+    }
+}
---
+++ MoneyLover/MoneyLover.UI/SignIn.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MoneyLover.UI
+{
+    /// <summary>
+    /// Interaction logic for SignIn.xaml
+    /// </summary>
+    public partial class SignIn : Window
+    {
+        public SignIn()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSignIn_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
